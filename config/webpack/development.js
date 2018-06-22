5d34a39ec5d8431f2091fea51045e88d2a5337d0
Hannes Wüthrich <hw@5px.ch>
2018-06-22 17:40:24 +0200

    Add database url

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const config = environment.toWebpackConfig()
 
+// Make it work with Chrome Debugger
 config.devtool = 'source-map'
 
 module.exports = config
